1b6ca4ef6ea3b503e041cbc7d6e351f67e6f449c
Hanno Jacobs <35495246+HannoJacobs@users.noreply.github.com>
2021-12-14 22:30:37 +0200

    1st order trendline works for btc and tsla

--- Stock_analysis.py
+++ Stock_analysis.py
@@ -1,26 +1,20 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
-start = '2020-01-01'
-end = datetime.today().strftime('%Y-%m-%d') # works
 
-tesla_price_chart = yf.download('tsla','2020-01-01', end)
-bitcoin_price_chart = yf.download('BTC-USD','2020-01-01', end)
-tesla = tesla_price_chart['Adj Close']
-btc = bitcoin_price_chart['Adj Close']
-numdays = len(tesla)
-print ("numdays = ", numdays)
-# t = np.linspace(0, numdays, 1)
-t = np.linspace(0, numdays, 1)
-print ("t = ", t)
+tesla_price_chart = yf.download('tsla', period="2y")
+btc_price_chart = yf.download('BTC-USD', period="2y")
+t = []
+tesla = []
+btc = []
+
+for i in range(len(tesla_price_chart)):
+    t.append(i)
+    tesla.append(tesla_price_chart['Adj Close'][i])
+    btc.append(btc_price_chart['Adj Close'][i])
 
 
-# tesla = yf.Ticker("TSLA")
-# dictionary = tesla.info
-# price = dictionary['currentPrice']
-# print ("\n", price, "\n")
-
+################## helper methods ##################
 def moving_average(days = 10):
     global tesla, btc
     
@@ -28,12 +22,29 @@ def moving_average(days = 10):
 
     return
 
+def make_trendline_data_order_1(trendline):
+    m = trendline[0]
+    c = trendline[1]
+    y = []
+    
+    for i in range(len(t)):
+        # y = m*t + c
+        y.append(m*i + c)
 
+    return y
 
+def make_trendline_data_order_2(trendline):
+    x2 = trendline[0]
+    x1 = trendline[1]
+    x0 = trendline[2]
+    y = []
+    
+    for i in range(len(t)):
+        # y = x0*i^2 + x1*i + x0
+        y.append(x0*(i**2) + x1*i + x0)
 
-
-
-
+    return y
+################## end helper methods ##################
 
 
 
@@ -42,12 +53,30 @@ fig, ax = plt.subplots(2, sharex=True)
 ##################
 ax[0].set_title('TSLA')
 ax[0].set_ylabel('USD ($)')
-# trendline1 = np.polyfit(int_date, y, 1)
 ax[0].plot(t, tesla)
-##################
+
+trendline1_1 = np.polyfit(t, tesla, 1)
+trend_line_array_1_1 = make_trendline_data_order_1(trendline1_1)
+ax[0].plot(t, trend_line_array_1_1)
+
+# trendline1_2 = np.polyfit(t, tesla, 2)
+# trend_line_array_1_2 = make_trendline_data_order_2(trendline1_2)
+# ax[0].plot(t, trend_line_array_1_2)
+
+# ##################
 ax[1].set_title('Bitcoin')
 ax[1].set_ylabel('USD ($)')
+ax[1].set_xlabel('Days')
 ax[1].plot(t, btc)
+
+trendline2_1 = np.polyfit(t, btc, 1)
+trend_line_array_2_1 = make_trendline_data_order_1(trendline2_1)
+ax[1].plot(t, trend_line_array_2_1)
+
+# trendline2 = np.polyfit(t, btc, 2)
+# trend_line_array_2 = make_trendline_data_order_1(trendline2)
+# ax[1].plot(t, trend_line_array_2)
+
 
 ax[0].grid()
 ax[1].grid()
