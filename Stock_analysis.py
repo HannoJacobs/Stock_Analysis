9562783f0deacd1c4486bd691480e93617e359e3
Hanno Jacobs <35495246+HannoJacobs@users.noreply.github.com>
2021-12-14 20:40:55 +0200

    plotting works

--- Stock_analysis.py
+++ Stock_analysis.py
@@ -2,11 +2,14 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 from datetime import datetime
 start = '2020-01-01'
 end = datetime.today().strftime('%Y-%m-%d') # works
+
+tesla_price_chart = yf.download('tsla','2020-01-01', end)
+bitcoin_price_chart = yf.download('BTC-USD','2020-01-01', end)
+tesla = tesla_price_chart['Adj Close']
+btc = bitcoin_price_chart['Adj Close']
 
 # tesla = yf.Ticker("TSLA")
 # dictionary = tesla.info
@@ -15,34 +18,26 @@ end = datetime.today().strftime('%Y-%m-%d') # works
 
 
 
-def plot_the_data():
-    # global tesla_price_chart, bitcoin_price_chart
-
-    tesla_price_chart = yf.download('tsla','2020-01-01', end)
-    bitcoin_price_chart = yf.download('BTC-USD','2020-01-01', end)
-
-    ####################### plots #######################
-    fig, ax = plt.subplots(2, sharex=True)
-    ax[0].set_title('TSLA')
-    ax[0].set_ylabel('USD ($)')
-    y = tesla_price_chart['Adj Close']
-    # trendline1 = np.polyfit(int_date, y, 1)
-    ax[0].plot(y)
-    ##################
-
-    ax[1].set_title('Bitcoin')
-    ax[1].set_ylabel('USD ($)')
-    y = bitcoin_price_chart['Adj Close']
-    ax[1].plot(y)
-
-
-    plt.show()
-    return
-
-
-
-plot_the_data()
 
 
 
 
+
+
+
+
+################## plots ##################
+fig, ax = plt.subplots(2, sharex=True)
+##################
+ax[0].set_title('TSLA')
+ax[0].set_ylabel('USD ($)')
+# trendline1 = np.polyfit(int_date, y, 1)
+ax[0].plot(tesla)
+##################
+ax[1].set_title('Bitcoin')
+ax[1].set_ylabel('USD ($)')
+ax[1].plot(btc)
+
+ax[0].grid()
+ax[1].grid()
+plt.show()
