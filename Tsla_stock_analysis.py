05dc43ff5a0d88c3914c5ff573918d968f6f1881
Hanno Jacobs <35495246+HannoJacobs@users.noreply.github.com>
2022-03-23 16:32:21 +0200

    removed and renamed files

--- Tsla_stock_analysis.py
+++ Tsla_stock_analysis.py
@@ -5,6 +5,16 @@ from scipy import signal
 import math
 from scipy.fft import fft, ifft
 
+################## write price ##################
+import yfinance as yf
+tesla_price_chart = yf.download('tsla', period="2y")
+tesla_file = open("tesla_share_price.txt", "w")
+for i in range(len(tesla_price_chart)):
+    val = tesla_price_chart['Adj Close'][i]
+    tesla_file.write(str(val)+"\n")
+tesla_file.close()
+################## end write price ##################
+
 
 tesla_file = open("tesla_share_price.txt", "r")
 rough_tesla_array = []
